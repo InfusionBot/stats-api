26f797703b1a5b510e5ffac5af19205ac7952972
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-12 21:05:10 +0530

    Update serverCount.js

--- src/routes/serverCount.js
+++ src/routes/serverCount.js
@@ -3,9 +3,19 @@
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
 const express = require("express");
+const mongoose = require("mongoose");
 
+const Guild = mongoose.model("Guild").schema;
 const router = express.Router();
+let servers;
+Guild.where({}).count((err,count)=>{
+    if(err) {console.error(err)}
+    else{
+        servers = count;
+    }
+})
 router.get("/", (req, res) => {
+    console.log(servers);
     res.status(200).send("This API is coming soon");
     res.end();
 });

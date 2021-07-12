496609e5866def5680f3abf2d34fe252477f5721
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-12 17:42:01 +0530

    Update apiLimiter.js

--- src/controllers/apiLimiter.js
+++ src/controllers/apiLimiter.js
@@ -1,3 +1,7 @@
+/**
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
 const rateLimit = require('express-rate-limit');
 
 module.exports = rateLimit({

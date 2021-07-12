5c68d537a99b9610afefbfc3b076b0066d5362fc
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-12 21:05:42 +0530

    Prepare for testing (#2)
    * deps: Add mongoose
    
    * api: Connect to MongoDB
    
    * Update format.yml
    
    * fix(deps): run npm install
    
    * Update .prettierignore
    
    * chore: 🎨 format code
    
    * api: Remove mongosee from serverCount.js
    
    * api: Add mongoose to app.js
    
    * Update serverCount.js
    
    * chore: 🎨 format code
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/app.js
+++ src/app.js
@@ -2,27 +2,43 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('./controllers/apiLimiter');
+// eslint-disable-next-line no-undef
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB!");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB: ", err);
+    });
+
+require("./controllers/apiLimiter");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use('/serverCount', require('nocache')());
+app.use("/serverCount", require("nocache")());
 
-app.use('/serverCount', require('./routes/serverCount'));
+app.use("/serverCount", require("./routes/serverCount"));
 
 app.use(function (req, res, next) {
-	console.log('Wrong ping 404');
-	res.status(404).send("Sorry can't find that!");
-	res.end();
+    console.log("Wrong ping 404");
+    res.status(404).send("Sorry can't find that!");
+    res.end();
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Running on port ${port}...`);
+    console.log(`Running on port ${port}...`);
 });
--- src/routes/serverCount.js
+++ src/routes/serverCount.js
@@ -2,9 +2,21 @@
  * Copyright (c) 2021 The Welcome-Bot Team and Contributors
  * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
  */
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+
+const Guild = mongoose.model("Guild").schema;
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.status(200).send("This API is coming soon");
-  res.end();
+let servers;
+Guild.where({}).count((err, count) => {
+    if (err) {
+        console.error(err);
+    } else {
+        servers = count;
+    }
+});
+router.get("/", (req, res) => {
+    console.log(servers);
+    res.status(200).send("This API is coming soon");
+    res.end();
 });

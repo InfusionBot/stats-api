319f6fd90ea520a3f35ed208086024afb6282aee
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-12 17:48:41 +0530

    Create serverCount.js

---
+++ src/routes/serverCount.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+const express = require('express');
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.send("This API is coming soon").status(200);
+});

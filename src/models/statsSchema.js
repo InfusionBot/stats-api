2cfd5cd1df47716b83b635ae334b8146dfcb9934
Puneet Gopinath <baalkrshna@gmail.com>
2021-07-13 07:52:04 +0530

    Create statsSchema.js

---
+++ src/models/statsSchema.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2021 The Welcome-Bot Team and Contributors
+ * Licensed under Lesser General Public License v2.1 (LGPl-2.1 - https://opensource.org/licenses/lgpl-2.1.php)
+ */
+const mongoose = require("mongoose");
+
+const stats = new mongoose.Schema({
+    guildCount: {
+        type: Number,
+        required: true,
+        trim: true,
+        default: 0,
+    },
+    shardCount: {
+        type: Number,
+        required: true,
+        trim: true,
+        default: 0,
+    },
+});
+
+module.exports = new mongoose.model("Stats", stats);
